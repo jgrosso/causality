005125d2474e77a1e2c3dec5324e181504a975f9
Joshua Grosso <jgrosso256@gmail.com>
2023-02-21 21:47:52 -0800

    Upload generated HTML for documentation

---
+++ docs.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+rm -rf docs
+mkdir docs
+
+agda --html --html-dir=docs --html-highlight=auto Causality/CausalGraph.lagda.md
+cp assets/{index.html,Agda.css} docs
+
+for file in docs/**/*.md
+do
+  pandoc "$file" -o "${file%.md}.html" --citeproc --bibliography=assets/bibliography/causality.bib --csl=assets/bibliography/ieee.csl --mathjax --css=Agda.css --standalone
+done
